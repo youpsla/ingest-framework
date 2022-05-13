8bf1a15ac688e078cd3a8e77cb0805ff4a8a188a
alainjabmo <aabraham@jabmo.com>
2022-05-13 16:14:04 +0200

    Add geographices metrics

--- sql_schemas/bing.sql
+++ sql_schemas/bing.sql
@@ -9,11 +9,11 @@ CREATE TABLE IF NOT EXISTS bing.accounts
 (
 	jab_id INT IDENTITY(1,1)
 	,jab_created_at TIMESTAMP WITHOUT TIME ZONE   ENCODE az64 default sysdate
-	,Id INTEGER   ENCODE az64
-	,Name VARCHAR(255)   ENCODE lzo
-	,Number VARCHAR(255)   ENCODE lzo
-	,AccountLifeCycleStatus VARCHAR(255)   ENCODE lzo
-	,PauseReason VARCHAR(255)   ENCODE lzo
+	,id INTEGER   ENCODE az64
+	,name VARCHAR(255)   ENCODE lzo
+	,number VARCHAR(255)   ENCODE lzo
+	,accountLifeCycleStatus VARCHAR(255)   ENCODE lzo
+	,pauseReason VARCHAR(255)   ENCODE lzo
 )
 DISTSTYLE AUTO
 ;
@@ -26,11 +26,11 @@ CREATE TABLE IF NOT EXISTS bing.campaigns
 (
 	jab_id INT IDENTITY(1,1)
 	,jab_created_at TIMESTAMP WITHOUT TIME ZONE   ENCODE az64 default sysdate
-	,Id INTEGER   ENCODE az64
-	,Name VARCHAR(255)   ENCODE lzo
-	,Status VARCHAR(255)   ENCODE lzo
-	,CampaignType VARCHAR(255)   ENCODE lzo
-	,AccountId INTEGER   ENCODE az64
+	,id INTEGER   ENCODE az64
+	,name VARCHAR(255)   ENCODE lzo
+	,status VARCHAR(255)   ENCODE lzo
+	,campaignType VARCHAR(255)   ENCODE lzo
+	,accountId INTEGER   ENCODE az64
 )
 DISTSTYLE AUTO
 ;
@@ -41,8 +41,8 @@ CREATE TABLE IF NOT EXISTS bing.adgroups
 (
 	jab_id INT IDENTITY(1,1)
 	,jab_created_at TIMESTAMP WITHOUT TIME ZONE   ENCODE az64 default sysdate
-	,Id BIGINT   ENCODE az64
-	,Name VARCHAR(255)   ENCODE lzo
+	,id BIGINT   ENCODE az64
+	,name VARCHAR(255)   ENCODE lzo
 	,Status VARCHAR(255)   ENCODE lzo
 	,CampaignId INTEGER   ENCODE az64
 )
@@ -108,6 +108,45 @@ CREATE TABLE IF NOT EXISTS bing.geo_metrics
 DISTSTYLE AUTO
 ;
 -- ALTER TABLE bing.geo_metrics owner to jabmo;
+
+CREATE TABLE IF NOT EXISTS bing.medias
+(
+	jab_id INT IDENTITY(1,1)
+	,jab_created_at TIMESTAMP WITHOUT TIME ZONE   ENCODE az64 default sysdate
+	,id BIGINT   ENCODE az64
+	,type VARCHAR(255)   ENCODE lzo
+	,url VARCHAR(255)   ENCODE lzo
+)
+DISTSTYLE AUTO
+;
+-- ALTER TABLE bing.medias owner to jabmo;
+
+
+CREATE TABLE IF NOT EXISTS bing.geographic_metrics
+(
+	jab_id INT IDENTITY(1,1)
+	,jab_created_at TIMESTAMP WITHOUT TIME ZONE   ENCODE az64 default sysdate
+	,time_period  DATE    ENCODE az64
+	,account_id BIGINT   ENCODE az64
+	,account_name VARCHAR(255)   ENCODE lzo
+	,adgroup_id BIGINT   ENCODE az64
+	,adgroup_name VARCHAR(255)   ENCODE lzo
+	,campaign_id BIGINT   ENCODE az64
+	,country VARCHAR(255)   ENCODE lzo
+	,state VARCHAR(255)   ENCODE lzo
+	,county VARCHAR(255)   ENCODE lzo
+	,city VARCHAR(255)   ENCODE lzo
+	,postal_code VARCHAR(255)   ENCODE lzo
+	,metro_area VARCHAR(255)   ENCODE lzo
+	,impressions INT ENCODE az64
+	,clicks INT ENCODE az64
+	,spend DECIMAL ENCODE az64
+)
+DISTSTYLE AUTO
+;
+-- ALTER TABLE bing.geographic_metrics owner to jabmo;
+
+
 
 
 
