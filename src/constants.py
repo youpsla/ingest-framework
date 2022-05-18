8f0a2b0dcd0172ad53c70874c73c8e8cdaa70591
alainjabmo <aabraham@jabmo.com>
2022-05-18 15:53:04 +0200

    Upgrade the way Redshift client collect Secret name containing connexion params

--- src/constants.py
+++ src/constants.py
@@ -8,3 +8,19 @@ UNPROCESSED_STATE = "unprocessed"
 REDSHIT_S3_ARN = "arn:aws:iam::467882466042:role/aa-ingest-dev-redshift-s3"
 
 
+DB_SECRET_NAMES = {
+    "redshift": {
+        "development": {
+            "readonly": "jabmo/db/redshift/web-ingest/dev/ro",
+            "readwrite": "jabmo/db/redshift/web-ingest/dev/rw",
+        },
+        "staging": {
+            "readonly": "jabmo/db/redshift/web-ingest/dev/ro",
+            "readwrite": "jabmo/db/redshift/web-ingest/dev/rw",
+        },
+        "prod": {
+            "readonly": "jabmo/db/redshift/web-ingest/prod",
+            "readwrite": "jabmo/db/redshift/web-ingest/prod/rw",
+        },
+    },
+}
