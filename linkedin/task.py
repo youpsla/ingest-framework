2122e4c5ee00b0dfa4266ba3b9ddd4176f819235
alainjabmo <aabraham@jabmo.com>
2022-05-22 10:18:23 +0200

    Remove try except

--- linkedin/task.py
+++ linkedin/task.py
@@ -55,7 +55,7 @@ class Task:
 
     @property
     def model(self):
-        return Model(self.params["model"], db_connection=self.db_connection)
+        return Model(self.params["model"], db_engine=self.db_connection)
 
     @property
     def actions(self):
@@ -177,7 +177,85 @@ class Task:
             traceback.print_exc()
             self.destination.write_results_db_connection.rollback()
 
+        if len(datas_from_source) == 0:
+            print(f"{self.name}: No new datas from source. Running next task.")
+            return "success"
+
+        if not datas_from_source:
+            print(
+                f"{self.name}: Failed retrieving datas from source. Skipping."
+                " Running next task."
+            )
             return "error"
+
+        # Insert datas in destination
+        if "insert" in self.actions:
+            datas_obj = []
+            for d in datas_from_source:
+                elem = d["datas"]
+                if elem is not None:
+                    m = Model(self.model.model_name, db_engine=self.db_connection)
+                    m.populate_values(elem)
+                    datas_obj.append(m)
+                    # del m
+            datas_values = []
+            # Search for new records and insert them.
+            if self.params["exclude_existing_in_db"]:
+                dede = self.model.get_all()
+                existing_ids = [
+                    r[self.params["exclude_key"]] for r in self.model.get_all()
+                ]
+                datas_obj = [
+                    r
+                    for r in datas_obj
+                    if getattr(
+                        r, self.params["destination_unique_key"]["value"]
+                    ).db_value
+                    not in existing_ids
+                ]
+                datas_values = [r.get_db_values_tuple() for r in datas_obj]
+            else:
+                datas_values = [r.get_db_values_tuple() for r in datas_obj]
+
+            print(
+                f"{self.name} - {self.model.model_name}:"
+                f" {len(datas_values)} record(s) will be inserted"
+            )
+            self.insert(datas_values)
+
+        if "update" in self.actions:
+            datas_obj = []
+            for d in datas_from_source:
+                if d["datas"] is not None:
+                    m = Model(
+                        self.model.model_name,
+                        db_connection=self.db_connection,
+                    )
+                    # m.set_field(
+                    #     self.params["db_query"]["fields"][0],
+                    #     m.params[self.params["db_query"]["fields"][0]],
+                    # )
+                    # m.set_field(
+                    #     d["where_field"],
+                    #     m.params[d["where_field"]],
+                    # )
+                    m.set_fields()
+                    m.populate_values(d["datas"])
+                    # setattr(getattr(m, d["where_field"]), "value", d["where_value"])
+
+                    where_dicts_list = []
+                    for v in self.params["db_query"]["keys"]:
+                        where_dicts_list.append({v: d["datas"][v]})
+
+                    values_dicts_list = []
+                    for v in self.params["db_query"]["fields"]:
+                        values_dicts_list.append({v[0]: d["datas"][v[1]]})
+
+                    self.update(
+                        values_dicts_list,
+                        where_dicts_list=where_dicts_list,
+                    )
+                    del m
 
     def insert(self, data):
         sql_query = SqlQuery(
