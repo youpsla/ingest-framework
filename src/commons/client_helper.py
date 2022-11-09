fb82b52754aac55c47d1b7483e720c516d4e62c8
alainjabmo <aabraham@jabmo.com>
2022-11-09 10:31:20 +0100

    remove unused S3 client

--- src/commons/client_helper.py
+++ src/commons/client_helper.py
@@ -2,15 +2,10 @@ from src.clients.bing.bing_client import BingAdsClient
 from src.clients.hubspot.hubspot_client import HubspotClient
 from src.clients.linkedin.linkedin_client import LinkedInClient
 from src.clients.redshift.redshift_client import RedshiftClient
-from src.clients.s3.s3_client import S3Client
 
 
 def get_client(env, client_name, task, db_connection=None):
-    if client_name == "s3":
-        client = S3Client(task=task, env=env)
-        client.name = client_name
-        return client
-    elif client_name == "bing":
+    if client_name == "bing":
         client = BingAdsClient(task=task, env=env)
         client.name = client_name
         return client
