ba765926593918cec231635acd9b118edff9ffd6
ritchy <rblezin@jabmo.com>
2022-11-22 17:00:00 +0100

    refactor(infrastructure.yaml): fix the json input

--- ci/deploy_infra.sh
+++ ci/deploy_infra.sh
@@ -8,17 +8,17 @@ if [[ "$1" =~ ^(development|staging|production)$ ]]; then
     BUCKET_NAME=jabmo-ingest
     TS=$(date +"%s")
     STACK_VERSION=$(git describe)-$1-$TS
-    echo "Deploying for $1 environment"
+    echo "==> Deploying for $1 environment <=="
     read -p "$(echo -e 'Continue?\n[Enter] → Yes\n[Ctrl]+[C] → No.\n ')"
     ############################################################################################
-    echo "Deploying/updating the infrastructure"
-    echo "Check if '$BUCKET_NAME' bucket exists"
+    echo "==> Deploying/updating the infrastructure <=="
+    echo "==> Check if '$BUCKET_NAME' bucket exists <=="
     if [[ ! -z $(aws s3api list-buckets --query "Buckets[?Name=='$BUCKET_NAME']" --output text) ]]; then
-        echo "Bucket '$BUCKET_NAME' Exists"
+        echo "==> Bucket '$BUCKET_NAME' Exists <=="
     else
-        echo "beginning of the creation of the bucket '$BUCKET_NAME'"
+        echo "==> beginning of the creation of the bucket '$BUCKET_NAME' <=="
         aws s3 mb s3://$BUCKET_NAME --region eu-west-1
-        echo "Bucket '$BUCKET_NAME' created"
+        echo "==> Bucket '$BUCKET_NAME' created <=="
     fi
     sam deploy -t ci/infrastructure.yaml \
         --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM \
@@ -27,11 +27,13 @@ if [[ "$1" =~ ^(development|staging|production)$ ]]; then
             Environment=$1 \
             SentryDsn=https://47e2fc82fc674f428f2b3e931eddace7@o1035237.ingest.sentry.io/6094713\
             StackVersion=$STACK_VERSION \
+        --confirm-changeset \
+        --use-json \
         --s3-bucket $BUCKET_NAME \
-        --stack-name jabmo-ingest-framework-$1
+        --stack-name jabmo-ingest-framework-$1 \
         --profile $AWS_PROFILE
     ############################################################################################
-    echo "Login to AWS docker repository: $CALLER_IDENTITY.dkr.ecr.eu-west-1.amazonaws.com"
+    echo "==> Login to AWS docker repository: $CALLER_IDENTITY.dkr.ecr.eu-west-1.amazonaws.com <=="
     aws ecr get-login-password \
         --region eu-west-1 \
         --profile $AWS_PROFILE \
@@ -39,10 +41,13 @@ if [[ "$1" =~ ^(development|staging|production)$ ]]; then
         --username AWS \
         --password-stdin $CALLER_IDENTITY.dkr.ecr.eu-west-1.amazonaws.com
     ############################################################################################
-    echo "building the image ingest-framework"
+    echo "==> building the image ingest-framework <=="
     docker build -t ingest-framework -f docker/ingest-framework.dockerfile . --platform linux/amd64
     ############################################################################################
-    echo "Pushing the image to $CALLER_IDENTITY.dkr.ecr.eu-west-1.amazonaws.com/ingest-framework-repository-$1"
+    echo "==> Tagging the image ingest-framework <=="
+    docker tag ingest-framework $CALLER_IDENTITY.dkr.ecr.eu-west-1.amazonaws.com/ingest-framework-repository-$1:$1
+    ############################################################################################
+    echo "==> Pushing the image to $CALLER_IDENTITY.dkr.ecr.eu-west-1.amazonaws.com/ingest-framework-repository-$1 <=="
     docker push $CALLER_IDENTITY.dkr.ecr.eu-west-1.amazonaws.com/ingest-framework-repository-$1:$1
 else
     echo "##############################################"
