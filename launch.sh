4ee5eeafde03fa6b8c2b0b77233f87f7f03be70c
alainjabmo <aabraham@jabmo.com>
2022-12-09 11:55:04 +0100

    V3-1216: Refactor lambda_f.
    Remove old unused lambda_f.py per provider
    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -15,15 +15,19 @@ handle_error() {
 case $PROVIDER in
 
     hubspot)
-        python hubspot_lambda_handler/src/lambda_f.py 2>&1
+        python src/scripts/lambda_f.py 2>&1
         ;;
 
     linkedin)
-        python linkedin/lambda_f.py 2>&1
+        python src/scripts/lambda_f.py 2>&1
         ;;
 
     bing)
-        python bing/src/lambda_f.py 2>&1
+        python src/scripts/lambda_f.py 2>&1
+        ;;
+
+    on24)
+        python src/scripts/lambda_f.py 2>&1
         ;;
 
     *)
