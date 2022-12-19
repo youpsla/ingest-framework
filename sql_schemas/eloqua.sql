71f325f35ed076d9a83ca817a83c0a29d3fb50ab
alainjabmo <aabraham@jabmo.com>
2022-12-19 21:12:30 +0100

    V3-1131: Works fine, accounts included V3-1333

--- sql_schemas/eloqua.sql
+++ sql_schemas/eloqua.sql
@@ -8,6 +8,21 @@ create table eloqua_development.clients
 );
 
 
+create table eloqua_development.accounts
+(
+    jab_id            integer   default "identity"(4116140, 0, '1,1'::text) encode az64,
+    jab_created_at    timestamp default ('now'::text)::timestamp without time zone encode az64,
+    client_name varchar(255) ENCODE lzo,
+    id VARCHAR(255)   ENCODE lzo,
+    name     varchar(255) ENCODE lzo,
+    address1 varchar(1024) ENCODE lzo,
+    city varchar(255) ENCODE lzo,
+    country varchar(255) ENCODE lzo,
+    created_at TIMESTAMP WITHOUT TIME ZONE   ENCODE az64,
+    updated_at TIMESTAMP WITHOUT TIME ZONE   ENCODE az64
+);
+
+
 --  Replace <schema_name> with the newly created
  GRANT USAGE ON SCHEMA eloqua_development TO  GROUP dev_rw;
  GRANT SELECT ON ALL TABLES IN SCHEMA eloqua_development TO GROUP dev_rw;
