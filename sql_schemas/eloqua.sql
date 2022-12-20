e313a088d15ec11cc90f2a7f357f712d118e7a87
alainjabmo <aabraham@jabmo.com>
2022-12-20 10:34:57 +0100

    V3-1134: Contacts working

--- sql_schemas/eloqua.sql
+++ sql_schemas/eloqua.sql
@@ -22,6 +22,23 @@ create table eloqua_development.accounts
     updated_at TIMESTAMP WITHOUT TIME ZONE   ENCODE az64
 );
 
+create table eloqua_development.contacts
+(
+    jab_id            integer   default "identity"(4116140, 0, '1,1'::text) encode az64,
+    jab_created_at    timestamp default ('now'::text)::timestamp without time zone encode az64,
+    client_name varchar(255) ENCODE lzo,
+    id VARCHAR(255)   ENCODE lzo,
+    name     varchar(255) ENCODE lzo,
+    email varchar(1024) ENCODE lzo,
+    first_name varchar(255) ENCODE lzo,
+    last_name varchar(255) ENCODE lzo,
+    title varchar(255) ENCODE lzo,
+    account_name varchar(255) ENCODE lzo,
+    account_id varchar(255) ENCODE lzo,
+    created_at TIMESTAMP WITHOUT TIME ZONE   ENCODE az64,
+    updated_at TIMESTAMP WITHOUT TIME ZONE   ENCODE az64
+);
+
 
 --  Replace <schema_name> with the newly created
  GRANT USAGE ON SCHEMA eloqua_development TO  GROUP dev_rw;
